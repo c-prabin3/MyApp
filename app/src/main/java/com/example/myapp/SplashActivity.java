f1e742aa41f71bbcf2d0cbef0e2c62bec3ca0795
Python <prabin.chaudhary3107@gmail.com>
2024-12-31 12:12:26 +0545

    This is my 1st commit

--- app/src/main/java/com/example/myapp/SplashActivity.java
+++ app/src/main/java/com/example/myapp/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -7,8 +8,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
     @Override
-    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Thread( ()-> {
+            try {
+                Thread.sleep(4000);
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }).start();
     }
 }
